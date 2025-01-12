ccac6ed47c4d7c67877e74632d4e6b1c8bc6ba3c
Jad <jadkuhail@gmail.com>
2025-01-12 10:58:46 +0200

    adjust Type.kt

--- presentation/src/main/java/com/jkuhail/shoppy/ui/theme/Type.kt
+++ presentation/src/main/java/com/jkuhail/shoppy/ui/theme/Type.kt
@@ -2,33 +2,93 @@ package com.jkuhail.shoppy.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.jkuhail.shoppy.R
+
+val SpaceMono = FontFamily(
+    Font(
+        resId = R.font.poppins_thin,
+        weight = FontWeight.Thin
+    ),
+    Font(
+        resId = R.font.poppins_extra_light,
+        weight = FontWeight.ExtraLight
+    ),
+    Font(
+        resId = R.font.poppins_light,
+        weight = FontWeight.Light
+    ),
+    Font(
+        resId = R.font.poppins_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.poppins_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.poppins_semi_bold,
+        weight = FontWeight.SemiBold
+    ),
+    Font(
+        resId = R.font.poppins_bold,
+        weight = FontWeight.Bold
+    ),
+    Font(
+        resId = R.font.poppins_extra_bold,
+        weight = FontWeight.ExtraBold
+    ),
+    Font(
+        resId = R.font.poppins_black,
+        weight = FontWeight.Black
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+    bodySmall = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = SpaceMono,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+    labelMedium = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.Bold,
+        fontSize = 20.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.Bold,
+        fontSize = 40.sp
+    ),
+    displayMedium = TextStyle(
+        fontFamily = SpaceMono,
+        fontWeight = FontWeight.Light,
+        fontSize = 20.sp
+    ),
 )
